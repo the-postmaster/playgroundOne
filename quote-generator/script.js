98318e5298e1f54af6d6d02245501ebbc0c25af8
Glen Naysmith <glen@postmastersolutions.co.nz>
2020-08-12 14:15:52 +1200

    quote-gen commit 3

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,1 +1,13 @@
-console.log('Hi losers');
+// Get Quote from API
+async function getQuote(){
+    const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+    const quoteURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    
+    try{
+        const response = await fetch(proxyURL + quoteURL);
+        const data = await response.json;
+    }
+    catch(error){
+        getQuote();
+    }
+}
