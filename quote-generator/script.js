40081cf83e26a05a55adeee11a6a78709c752e0c
Glen Naysmith <glen@postmastersolutions.co.nz>
2020-08-12 14:42:03 +1200

    quote-gen commit 4

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteAuthor = document.getElementById('author');
+const quoteText = document.getElementById('quote');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get Quote from API
 async function getQuote(){
     const proxyURL = 'https://cors-anywhere.herokuapp.com/';
@@ -5,9 +11,22 @@ async function getQuote(){
     
     try{
         const response = await fetch(proxyURL + quoteURL);
-        const data = await response.json;
+        const data = await response.json();
+        
+        //If author is blank, add 'Unknown'
+        if (data.quoteAuthor === ''){
+            quoteAuthor.innerText = 'Unknown';
+        } else {
+            quoteAuthor.innerText = data.quoteAuthor;
+        }
+
+        quoteText.innerText = data.quoteText;
+    
     }
     catch(error){
+        console.log('whoops ' + error)
         getQuote();
     }
 }
+
+getQuote();
